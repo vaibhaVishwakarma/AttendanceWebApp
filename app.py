a9444a9711137bcde22df3b404b8fafd4dcde76d
vaibhaVishwakarma <vaibhavofficial709@gmail.com>
2024-07-04 03:29:33 +0530

    draggable tags

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from typing import List
 from fastapi import FastAPI, Form, Request 
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel 
 app=FastAPI()
 # Allow all origins for simplicity (configure this appropriately for production)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,16 +15,21 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+class DataItem(BaseModel):
+    values: List[List[str]]
+
+class ResponseModel(BaseModel):
+    status: str
+    data_received: List[List[str]]
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory = "templates")
 
 @app.get("/",response_class = HTMLResponse)
 async def getpage(request : Request):
     return templates.TemplateResponse("./index.html ",{"request":request}) 
-@app.post("/submitTable")
-
-async def handle_form_submission(request: Request):
-    form_data = await request.form()
-    data_dict = {key: value for key, value in form_data.items()}
-    return data_dict
+@app.post("/submit" , response_model=ResponseModel)
+async def handle_form_submission(data:DataItem):
+    print("hello ",data)
+    return JSONResponse({"name":"success"})
     
